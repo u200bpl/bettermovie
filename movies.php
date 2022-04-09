7915c5bc5121948732e169a384db65010423e211
u200b <u200b@doverman.nl>
2022-04-10 01:51:38 +0200

    codie

--- movies.php
+++ movies.php
@@ -1,21 +1,22 @@
 <?php include 'layout/header.php' ?>
 
-<?php
-    $id = $_GET['id'];
+    <?php
+        $id = $_GET['id'];
 
-    // Query
-    require_once './backend/conn.php';
-    $query = "SELECT * FROM movies WHERE id = :id";
-    $statement = $conn->prepare($query);
-    $statement->execute([":id" => $id]);
-    $movielist = $statement->fetchAll(PDO::FETCH_ASSOC);
-?>
+        // Query
+        require_once './backend/conn.php';
+        $query = "SELECT * FROM movies WHERE id = :id";
+        $statement = $conn->prepare($query);
+        $statement->execute([":id" => $id]);
+        $movielist = $statement->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+</header>
 
 <?php foreach($movielist as $movie): ?>
     <p><?php echo $movie['id']; ?></p>
     <p><?php echo $movie['title']; ?></p>
     <p><?php echo $movie['genre']; ?></p>
-    <img src="style/img/<?php echo $movie['banner']; ?>.jpg" alt="<?php echo $movie['title']; ?> img banner">
+    <!-- <img src="style/img/<?php echo $movie['banner']; ?>.jpg" alt="<?php echo $movie['title']; ?> img banner"> -->
     <p><?php echo $movie['year']; ?></p>
     <p>Trailer</p>
     <iframe src="https://databasegdriveplayer.co/player.php?imdb=<?php echo $movie['iframe']; ?>" frameborder="0" width="100%" height="400" allowfullscreen="1"> </iframe>
--- style/js/main.js
+++ style/js/main.js
@@ -4,5 +4,5 @@ var searchBox = document.getElementsByClassName("search-box")[0];
 searchIcon.addEventListener("click", activateSearch);
 
 function activateSearch() {
-  searchBox.classList.toggle("active");
+    searchBox.classList.toggle("active");
 }

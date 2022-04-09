e7d33bdb59790e7e4338394c747be6361c361c17
u200b <u200b@doverman.nl>
2022-04-09 19:24:53 +0200

    added more code

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <main>
     <div class="wrapper">
         <div class="rows">
+            <?php for ($i = 0; $i < 8; $i++) { ?>
             <div class="movie">
-                <img src="style/img/freeguy.jpg" alt="">
+                <a href="movies.php"><img src="style/img/freeguy.jpg" alt="Freeguy img banner"></a>
+                <a href="movies.php"><p>Free Guy</p></a>
             </div>
+            <?php } ?>
         </div>
     </div>
 </main>

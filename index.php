f1e7f88175caf2d53b767e0785eac1ecffab61d9
u200b <u200b@doverman.nl>
2022-04-09 18:11:03 +0200

    added freeguy

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php include 'layout/header.php' ?>
 
 <main>
-    
+    <div class="wrapper">
+        <div class="rows">
+            <div class="movie">
+                <img src="style/img/freeguy.jpg" alt="">
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php include 'layout/footer.php' ?>

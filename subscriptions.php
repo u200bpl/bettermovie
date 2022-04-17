fa05190e1ea032dd1d35c8da3044104f42cd10ee
geitjoch <101874339+u200bpl@users.noreply.github.com>
2022-04-17 23:19:55 +0200

    Added subs

--- subscriptions.php
+++ subscriptions.php
@@ -1,21 +1,93 @@
 <?php
     require_once 'head.php';
-    require_once 'header.php';
     require_once 'backend/config.php';
-?>
-        <section class="hero" style="background-image: url(style/img/.jpg);">
-            <div class="heroinformation">
 
+    session_start();
+    if(isset($_SESSION['user_id'])) {
+        header("Location: index.php");
+        exit;
+    }
+?>
+<header>
+    <div class="navbartest">
+        <nav>
+            <div class="wrapper">
+                <div class="navbar">
+                    <div class="navleft">
+                        <a href="index.php"><h1><span>Better</span>movie</h1></a>
+                        <a href="subscriptions.php" class="active">Subscriptions</a>
+                    </div>
+
+                    <div class="navright">
+                        <div class="lgn-btn">
+                            <a href="login/">Sign In</a>   
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        </nav>
+    </div>
+
+    <div class="spacer">.</div>
 </header>
 
 <main>
     <!-- 
-        RECENTLTY ADDED
+        SUB PLANS
     -->
     <div class="wrapper">
-        
+        <div class="plans">
+            <div class="plan">
+                <div class="planheader">
+                    <h5>Free</h5>
+                    <p><span>€</span> 0,-</p>
+                </div>
+                <div class="planinformation">
+                    <ul>
+                        <li>Resolution: <span>480p</span></li>
+                        <li>Access: <span>Limited</span></li>
+                        <li>Devices: <span>1</span></li>
+                        <li> <span></span></li>
+                    </ul>
+
+                    <div class="prch-btn"><button>Purchase</button></div>
+                </div>
+            </div>
+
+            <div class="plan">
+                <div class="planheader">
+                    <h5>Basic</h5>
+                    <p><span>€</span> 4,99</p>
+                </div>
+                <div class="planinformation">
+                    <ul>
+                        <li>Resolution:<span> 1080p</span></li>
+                        <li>Access: <span>Unlimited</span></li>
+                        <li>Devices: <span>2</span></li>
+                        <li> <span></span></li>
+                    </ul>
+
+                    <div class="prch-btn"><button>Purchase</button></div>
+                </div>
+            </div>
+
+            <div class="plan">
+                <div class="planheader">
+                    <h5>Ultimate</h5>
+                    <p><span>€</span> 14,99</p>
+                </div>
+                <div class="planinformation">
+                    <ul>
+                        <li>Resolution:<span> 4K+HDR</span></li>
+                        <li>Access: <span>Unlimited</span></li>
+                        <li>Devices: <span>6</span></li>
+                        <li> <span></span></li>
+                    </ul>
+
+                    <div class="prch-btn"><button>Purchase</button></div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 

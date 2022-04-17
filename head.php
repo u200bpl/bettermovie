f89756af5a2f4981af707b1e9806c1542a5139ae
geitjoch <101874339+u200bpl@users.noreply.github.com>
2022-04-17 20:11:45 +0200

    Codie

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://kit.fontawesome.com/268c8277db.js" crossorigin="anonymous"></script>
 
-<title>Bettermovie | <?php echo basename($_SERVER['PHP_SELF']); ?></title>
+<title>Bettermovie | <?php echo basename($_SERVER['PHP_SELF'],'.php'); ?></title>
 
 <link rel="stylesheet" href="<?php echo $base_url; ?>/style/css/style.css">
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@clappr/player@latest/dist/clappr.min.js"></script>
